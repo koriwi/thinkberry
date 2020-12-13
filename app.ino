1826cca6601151f857ef1a8421ff631d67a874e2
Kilian Gosewisch <kilian2798@gmail.com>
2020-12-14 00:29:18 +0100

    works so so

--- app.ino
+++ app.ino
@@ -1,6 +1,6 @@
 #ifdef __arm__
 // should use uinstd.h to define sbrk but Due causes a conflict
-extern "C" char* sbrk(int incr);
+extern 'C' char* sbrk(int incr);
 #else  // __ARM__
 extern char *__brkval;
 #endif  // __arm__
@@ -28,9 +28,9 @@ bool keys[colCount][rowCount];
 bool lastValue[colCount][rowCount];
 bool changedValue[colCount][rowCount];
 
-char keyboard[colCount][rowCount];
-char keyboard_symbol[colCount][rowCount];
-// uint8_t keyboard_alt[colCount][rowCount];
+uint8_t keyboard[colCount][rowCount];
+uint16_t keyboard_symbol[colCount][rowCount];
+uint16_t keyboard_alt[colCount][rowCount];
 
 
 bool symbolSelected;
@@ -38,134 +38,134 @@ bool altSelected;
 void setup() {
     Serial.begin(115200);
     Keyboard.begin();
-    delay(5000);
-    keyboard[0][0] = "q";
-    keyboard[0][1] = "w";
+    keyboard[0][0] = KEY_Q;
+    keyboard[0][1] = KEY_W;
     keyboard[0][2] = NULL; // symbol
-    keyboard[0][3] = "a";
+    keyboard[0][3] = KEY_A;
     keyboard[0][4] = NULL; // ALT
-    keyboard[0][5] = " ";
+    keyboard[0][5] = KEY_SPACE;
     keyboard[0][6] = NULL; // Mic
     
-    keyboard[1][0] = "e";
-    keyboard[1][1] = "s";
-    keyboard[1][2] = "d";
-    keyboard[1][3] = "p";
-    keyboard[1][4] = "x";
-    keyboard[1][5] = "y";
+    keyboard[1][0] = KEY_E;
+    keyboard[1][1] = KEY_S;
+    keyboard[1][2] = KEY_D;
+    keyboard[1][3] = KEY_P;
+    keyboard[1][4] = KEY_X;
+    keyboard[1][5] = KEY_Y;
     keyboard[1][6] = NULL; // Left Shift
     
-    keyboard[2][0] = "r";
-    keyboard[2][1] = "g";
-    keyboard[2][2] = "t";
-    keyboard[2][3] = NULL; // Right Shit
-    keyboard[2][4] = "t";
-    keyboard[2][5] = "c";
-    keyboard[2][6] = "f";
+    keyboard[2][0] = KEY_R;
+    keyboard[2][1] = KEY_G;
+    keyboard[2][2] = KEY_T;
+    keyboard[2][3] = NULL; // Right Shift
+    keyboard[2][4] = KEY_V;
+    keyboard[2][5] = KEY_C;
+    keyboard[2][6] = KEY_F;
     
-    keyboard[3][0] = "u";
-    keyboard[3][1] = "h";
-    keyboard[3][2] = "z";
+    keyboard[3][0] = KEY_U;
+    keyboard[3][1] = KEY_H;
+    keyboard[3][2] = KEY_Z;
     keyboard[3][3] = NULL; // Enter
-    keyboard[3][4] = "b";
-    keyboard[3][5] = "n";
-    keyboard[3][6] = "j";
+    keyboard[3][4] = KEY_B;
+    keyboard[3][5] = KEY_N;
+    keyboard[3][6] = KEY_J;
     
-    keyboard[4][0] = "o";
-    keyboard[4][1] = "l";
-    keyboard[4][2] = "i";
+    keyboard[4][0] = KEY_O;
+    keyboard[4][1] = KEY_L;
+    keyboard[4][2] = KEY_I;
     keyboard[4][3] = NULL; // Backspace
     keyboard[4][4] = NULL; // Dollar used as Ctrl
-    keyboard[4][5] = "m";
-    keyboard[4][6] = "k";
-    Serial.println(freeMemory());
-    keyboard_symbol[0][0] = "#";
-    keyboard_symbol[0][1] = "1";
+    keyboard[4][5] = KEY_M;
+    keyboard[4][6] = KEY_K;
+    
+    keyboard_symbol[0][0] = KEY_HASHTAG;
+    keyboard_symbol[0][1] = KEY_1;
     keyboard_symbol[0][2] = NULL;
-    keyboard_symbol[0][3] = "}";
+    keyboard_symbol[0][3] = KEY_PLUS|MOD_LEFT_SHIFT;
     keyboard_symbol[0][4] = NULL;
     keyboard_symbol[0][5] = NULL;
-    keyboard_symbol[0][6] = "0";
+    keyboard_symbol[0][6] = KEY_0;
     
-    keyboard_symbol[1][0] = "2";
-    keyboard_symbol[1][1] = "4";
-    keyboard_symbol[1][2] = "5";
-    keyboard_symbol[1][3] = "@";
-    keyboard_symbol[1][4] = "8";
-    keyboard_symbol[1][5] = "7";
+    keyboard_symbol[1][0] = KEY_2;
+    keyboard_symbol[1][1] = KEY_4;
+    keyboard_symbol[1][2] = KEY_5;
+    keyboard_symbol[1][3] = KEY_Q|MOD_RIGHT_ALT;
+    keyboard_symbol[1][4] = KEY_8;
+    keyboard_symbol[1][5] = KEY_7;
     keyboard_symbol[1][6] = NULL;
     
-    keyboard_symbol[2][0] = "3";
-    keyboard_symbol[2][1] = "/";
-    keyboard_symbol[2][2] = "(";
+    keyboard_symbol[2][0] = KEY_3;
+    keyboard_symbol[2][1] = KEY_7|MOD_LEFT_SHIFT;
+    keyboard_symbol[2][2] = KEY_8|MOD_LEFT_SHIFT;
     keyboard_symbol[2][3] = NULL;
-    keyboard_symbol[2][4] = "_";
-    keyboard_symbol[2][5] = "9";
-    keyboard_symbol[2][6] = "6";
+    keyboard_symbol[2][4] = KEY_SZ|MOD_LEFT_SHIFT;
+    keyboard_symbol[2][5] = KEY_9;
+    keyboard_symbol[2][6] = KEY_6;
     
-    keyboard_symbol[3][0] = (int)'?' << 8;
-    keyboard_symbol[3][1] = (int)'>' << 8;
-    keyboard_symbol[3][2] = (int)'(' << 8;
+    keyboard_symbol[3][0] = KEY_MINUS|MOD_LEFT_SHIFT;
+    keyboard_symbol[3][1] = KEY_PERIOD|MOD_LEFT_SHIFT;
+    keyboard_symbol[3][2] = KEY_9|MOD_LEFT_SHIFT;
     keyboard_symbol[3][3] = NULL;
-    keyboard_symbol[3][4] = (int)'!' << 8;
-    keyboard_symbol[3][5] = (int)',' << 8;
-    keyboard_symbol[3][6] = (int)'<' << 8;
+    keyboard_symbol[3][4] = KEY_1|MOD_LEFT_SHIFT;
+    keyboard_symbol[3][5] = KEY_COMMA;
+    keyboard_symbol[3][6] = KEY_COMMA|MOD_LEFT_SHIFT;
     
-    keyboard_symbol[4][0] = (int)']' << 8;
-    keyboard_symbol[4][1] = (int)'@' << 8;
-    keyboard_symbol[4][2] = (int)'/' << 8;
+    keyboard_symbol[4][0] = KEY_PLUS;
+    keyboard_symbol[4][1] = KEY_2|MOD_LEFT_SHIFT;
+    keyboard_symbol[4][2] = KEY_MINUS;
     keyboard_symbol[4][3] = NULL;
-    keyboard_symbol[4][4] = (int)"-" << 8;
-    keyboard_symbol[4][5] = (int)'.' << 8;
-    keyboard_symbol[4][6] = (int)'|' << 8;
+    keyboard_symbol[4][4] = NULL;
+    keyboard_symbol[4][5] = KEY_PERIOD;
+    keyboard_symbol[4][6] = KEY_HASHTAG|MOD_LEFT_SHIFT;
 
-    // keyboard_alt[0][0] = NULL;
-    // keyboard_alt[0][1] = KEY_F1;
-    // keyboard_alt[0][2] = NULL;
-    // keyboard_alt[0][3] = NULL;
-    // keyboard_alt[0][4] = NULL;
-    // keyboard_alt[0][5] = NULL;
-    // keyboard_alt[0][6] = KEY_F10;
+    keyboard_alt[0][0] = NULL;
+    keyboard_alt[0][1] = KEY_F1;
+    keyboard_alt[0][2] = NULL;
+    keyboard_alt[0][3] = NULL;
+    keyboard_alt[0][4] = NULL;
+    keyboard_alt[0][5] = NULL;
+    keyboard_alt[0][6] = KEY_F10;
     
-    // keyboard_alt[1][0] = KEY_F2;
-    // keyboard_alt[1][1] = KEY_F4;
-    // keyboard_alt[1][2] = KEY_F5;
-    // keyboard_alt[1][3] = KEY_LEFT_GUI;
-    // keyboard_alt[1][4] = KEY_F8;
-    // keyboard_alt[1][5] = KEY_F7;
-    // keyboard_alt[1][6] = NULL;
+    keyboard_alt[1][0] = KEY_F2;
+    keyboard_alt[1][1] = KEY_F4;
+    keyboard_alt[1][2] = KEY_F5;
+    keyboard_alt[1][3] = KEY_LEFT_GUI;
+    keyboard_alt[1][4] = KEY_F8;
+    keyboard_alt[1][5] = KEY_F7;
+    keyboard_alt[1][6] = NULL;
     
-    // keyboard_alt[2][0] = KEY_F3;
-    // keyboard_alt[2][1] = NULL;
-    // keyboard_alt[2][2] = KEY_TAB;
-    // keyboard_alt[2][3] = NULL;
-    // keyboard_alt[2][4] = NULL;
-    // keyboard_alt[2][5] = KEY_F9;
-    // keyboard_alt[2][6] = KEY_F6;
+    keyboard_alt[2][0] = KEY_F3;
+    keyboard_alt[2][1] = KEY_8|MOD_RIGHT_ALT;
+    keyboard_alt[2][2] = KEY_7|MOD_RIGHT_ALT;
+    keyboard_alt[2][3] = NULL;
+    keyboard_alt[2][4] = NULL;
+    keyboard_alt[2][5] = KEY_F9;
+    keyboard_alt[2][6] = KEY_F6;
     
-    // keyboard_alt[3][0] = NULL;
-    // keyboard_alt[3][1] = NULL;
-    // keyboard_alt[3][2] = NULL;
-    // keyboard_alt[3][3] = NULL;
-    // keyboard_alt[3][4] = NULL;
-    // keyboard_alt[3][5] = NULL;
-    // keyboard_alt[3][6] = KEY_LEFT_ARROW;
-    // keyboard_alt[4][0] = NULL;
-    // keyboard_alt[4][1] = KEY_RIGHT_ARROW;
-    // keyboard_alt[4][2] = KEY_UP_ARROW;
-    // keyboard_alt[4][3] = NULL;
-    // keyboard_alt[4][4] = NULL;
-    // keyboard_alt[4][5] = NULL;
-    // keyboard_alt[4][6] = KEY_DOWN_ARROW;
+    keyboard_alt[3][0] = NULL;
+    keyboard_alt[3][1] = KEY_9|MOD_RIGHT_ALT;
+    keyboard_alt[3][2] = KEY_0|MOD_RIGHT_ALT;
+    keyboard_alt[3][3] = NULL;
+    keyboard_alt[3][4] = NULL;
+    keyboard_alt[3][5] = NULL;
+    keyboard_alt[3][6] = KEY_LEFT_ARROW;
+
+    keyboard_alt[4][0] = NULL;
+    keyboard_alt[4][1] = KEY_RIGHT_ARROW;
+    keyboard_alt[4][2] = KEY_UP_ARROW;
+    keyboard_alt[4][3] = NULL;
+    keyboard_alt[4][4] = NULL;
+    keyboard_alt[4][5] = NULL;
+    keyboard_alt[4][6] = KEY_DOWN_ARROW;
   
  
     for(int x=0; x<rowCount; x++) {
-        Serial.print(rows[x]); Serial.println(" as input");
+        Serial.print(rows[x]); Serial.println(' as input');
         pinMode(rows[x], INPUT);
     }
  
     for (int x=0; x<colCount; x++) {
-        Serial.print(cols[x]); Serial.println(" as input-pullup");
+        Serial.print(cols[x]); Serial.println(' as input-pullup');
         pinMode(cols[x], INPUT_PULLUP);
     }
 
@@ -221,7 +221,7 @@ bool keyActive(int colIndex, int rowIndex) {
 }
 
 bool isPrintableKey(int colIndex, int rowIndex) {
-  return keyboard_symbol[colIndex][rowIndex] != NULL || keyboard[colIndex][rowIndex] != NULL;
+  return keyboard_symbol[colIndex][rowIndex] != NULL || keyboard[colIndex][rowIndex] != NULL || keyboard_alt[colIndex][rowIndex] != NULL;
 }
 
 void printMatrix() {
@@ -229,35 +229,49 @@ void printMatrix() {
         for (int colIndex=0; colIndex < colCount; colIndex++) {
           // we only want to print if the key is pressed and it is a printable character
           if (keyPressed(colIndex, rowIndex) && isPrintableKey(colIndex, rowIndex)) {
-            
-            if (keyActive(0, 4)) {
-              // Keyboard.press(KeyboardKeycode(keyboard_alt[colIndex][rowIndex]));
-              // delay(10);
-              // Keyboard.releaseAll();
+
+            uint16_t input;
+            if(keyActive(0, 4)) {
+              Serial.println("UFF");
+              input = keyboard_alt[colIndex][rowIndex];
+            } else if(keyActive(1, 6) || keyActive(2, 3)) {
+              Keyboard.press(KeyboardKeycode(KEY_LEFT_SHIFT));
+              input = keyboard[colIndex][rowIndex];
+            } else if(keyActive(0, 2)) {
+              input = keyboard_symbol[colIndex][rowIndex];
             } else {
-              char toPrint;
-              if (symbolSelected) {
-                symbolSelected = false;
-                toPrint = keyboard_symbol[colIndex][rowIndex];
-              } else {
-                toPrint = keyboard[colIndex][rowIndex];
-              }
-              // keys 1,6 and 2,3 are Shift keys, so we want to upper case
-              // if (keyActive(1,6) || keyActive(2,3)) {
-              //   toPrint.toUpperCase();
-              // }
-              
-              // if(toPrint <= 255) {
-                Serial.println(KeyboardKeycode((int)"g"));
-                Serial.println(KeyboardKeycode(keyboard[colIndex][rowIndex]));
-                Keyboard.print(String(keyboard[colIndex][rowIndex]));
-                // delay(10);
-                // Keyboard.releaseAll();
-              // } else {
-                // Serial.println(toPrint >> 8);
-                // Serial.println( (int)'@');
-                // Keyboard.print((char)(toPrint >> 8));
-              // }
+              input = keyboard[colIndex][rowIndex];
+            }
+
+            if(input <= 255) {
+              Serial.println("NORMAL");
+              Serial.println(input);
+              Keyboard.write(KeyboardKeycode(input));
+              Keyboard.releaseAll();
+            } else if(input >= MOD_LEFT_CTRL && input < MOD_LEFT_SHIFT) {
+              Serial.println("CTRL_L");
+              Keyboard.press(KeyboardKeycode(KEY_LEFT_CTRL));
+              Keyboard.press(KeyboardKeycode(input - MOD_LEFT_CTRL));
+              delay(10);
+              Keyboard.releaseAll();
+            } else if(input >= MOD_LEFT_SHIFT && input < MOD_LEFT_ALT) {
+              Serial.println("SHIFT_L");
+              Keyboard.press(KeyboardKeycode(KEY_LEFT_SHIFT));
+              Keyboard.press(KeyboardKeycode(input - MOD_LEFT_SHIFT));
+              delay(10);
+              Keyboard.releaseAll();
+            } else if(input >= MOD_LEFT_ALT && input < MOD_LEFT_GUI) {
+              Serial.println("ALT_L");
+              Keyboard.press(KeyboardKeycode(KEY_LEFT_ALT));
+              Keyboard.press(KeyboardKeycode(input - MOD_LEFT_ALT));
+              delay(10);
+              Keyboard.releaseAll();
+            } else if(input >= MOD_RIGHT_ALT && input < MOD_RIGHT_GUI) {
+              Serial.println("ALT_R");
+              Keyboard.press(KeyboardKeycode(KEY_RIGHT_ALT));
+              Keyboard.press(KeyboardKeycode(input - MOD_RIGHT_ALT));
+              delay(10);
+              Keyboard.releaseAll();
             }
           }
         }   
